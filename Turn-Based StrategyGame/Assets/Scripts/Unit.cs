09221cab2a9f8cd412120415a3b679be0170117f
NgoTheVinh <vinhnthe170433@fpt.edu.vn>
2025-02-12 16:36:24 +0700

    Adding Unit Animation, adding Select Unit mechanics/Visual

--- Turn-Based StrategyGame/Assets/Scripts/Unit.cs
+++ Turn-Based StrategyGame/Assets/Scripts/Unit.cs
@@ -9,10 +9,15 @@ public class Unit : MonoBehaviour
     private Vector3 targetPosition;
     [SerializeField] private float MoveSpeed = 4.0f;
     private float stoppingDistance = .1f;
+    [SerializeField] private Animator unitAnimator;
+    private float rotatingSpeed = 5f;
 
+    private void Awake()
+    {
+        targetPosition = transform.position;
+    }
 
-
-    private void Move(Vector3 targetPosition)
+    public void Move(Vector3 targetPosition)
     {
         this.targetPosition = targetPosition;
     }
@@ -24,10 +29,14 @@ public class Unit : MonoBehaviour
             Vector3 moveDir = (targetPosition - transform.position).normalized;
 
             transform.position += moveDir * Time.deltaTime * MoveSpeed;
+
+            transform.forward = Vector3.Lerp(transform.forward, moveDir, Time.deltaTime * rotatingSpeed);
+
+            unitAnimator.SetBool("IsWalking", true);
         }
-        if (Input.GetMouseButtonDown(0))
+        else
         {
-            Move(MouseWorld.GetMousePosition());
+            unitAnimator.SetBool("IsWalking", false);
         }
 
     }
